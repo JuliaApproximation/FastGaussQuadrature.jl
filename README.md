6f9e8e708a2ae30d72c0caa41257f70c08207c58
AT <townsend@maths.ox.ac.uk>
2014-08-31 10:56:56 -0400

    Adding in code for Gauss-Legendre nodes and weights.

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 FastGauss
 =========
 
-Compute Gauss nodes and weights in O(n) complexity. 
+  OUR AIM: To be the fastest Julia code to compute 16-digit accurate Gauss 
+           nodes and weights (without tabulation). We have only Gauss-Legendre now. 
+
+  GAUSS-LEGENDRE EXAMPLE: 
+          tic(), GaussLegendre( 1000 ); toc()
+          elapsed time: 0.000829828 seconds
+
+          tic(), GaussLegendre( 1000000 ); toc()
+          elapsed time: 0.310792745 seconds
+
+ For n<=5: Use an analytic expression. 
+ For n<=60: Use Newton's method to solve P_n(x)=0. Weights are related to P'_n(x), see [2].
+ For n>60: Use asymptotic expansions for the Legendre nodes and weights, see [1].
+
+ FOR MORE INFORMATION ON THE UNDERLYING ALGORITHMS SEE: 
+ 
+ REFERENCES:
+   [1] I. Bogaert, "Iteration-free computation of Gauss-Legendre quadrature
+       nodes and weights", SIAM J. Sci. Comput., 36(3), A1008-A1026, 2014.
+       (Describes the algorithm implemented here for n>60. A few modifications were made 
+         to optimize for Julia. )
+
+   [2] N. Hale and A. Townsend, "Fast computation of Gauss-Jacobi quadrature 
+       nodes and weights", SIAM J. Sci. Comput., 2012.
+       (Describes the algorithm implemented here for 5<n<=60 and an O(n) algorithm for 
+        computing Gauss-Jacobi nodes and weights.)
+
+  For a review and comparison of the recent methods for computing Gauss nodes, see [2].  
+
---
+++ src/GaussLegendre.jl
@@ -0,0 +1,231 @@
+function GaussLegendre( n::Int64 )
+# GAUSSLEGENDRE(n)  COMPUTE THE GAUSS-LEGENDRE NODES AND WEIGHTS IN O(n) time.
+
+if n < 0
+    x = (Float64[],Float64[])
+elseif n == 0 
+    x = (Float64[],Float64[])
+elseif n == 1
+    x = (0.0,2)
+elseif n == 2
+    x = Array(Float64,2)
+    w = Array(Float64,2)
+    x[1] = -sqrt(3); 
+    x[2] = sqrt(3)
+    w[1] = 1.0 
+    w[2] =  1.0
+    x = (x,w)
+elseif n == 3
+    x = Array(Float64,3)
+    w = Array(Float64,3)
+    x[1] = -sqrt(3/5) 
+    x[2] = 0 
+    x[3] = sqrt(3/5) 
+    w[1] = 5/9
+    w[2] = 8/9
+    w[3] = 5/9
+    x = (x,w)
+elseif n == 4 
+    x = Array(Float64,4)
+    w = Array(Float64,4)
+    const a::Float64 = 2/7*sqrt(6/5)
+    x[1] = -sqrt(3/7+a)
+    x[2] = -sqrt(3/7-a)
+    x[3] =  sqrt(3/7-a)
+    x[4] =  sqrt(3/7+a)
+    w[1] = (18 - sqrt(30))/36;
+    w[2] = (18 + sqrt(30))/36;
+    w[3] = (18 + sqrt(30))/36;
+    w[4] = (18 - sqrt(30))/36;
+    x = (x, w)
+elseif n == 5
+    x = Array(Float64, 5)
+    w = Array(Float64, 5)
+    const b::Float64 = 2sqrt(10/7)
+    x[1] = -sqrt(5+b)/3 
+    x[2] = -sqrt(5-b)/3
+    x[3] = 0
+    x[4] = sqrt(5-b)/3 
+    x[5] = sqrt(5+b)/3
+    w[1] = (322-13sqrt(70))/900
+    w[2] = (322+13sqrt(70))/900
+    w[3] = 128/225
+    w[4] = (322+13sqrt(70))/900
+    w[5] = (322-13sqrt(70))/900
+    x = (x, w);
+elseif n <= 60 
+# NEWTON'S METHOD WITH THREE-TERM RECURRENCE:   
+    x = rec( n::Int64 )
+else
+# USE ASYMPTOTIC EXPANSIONS:
+    x = asy( n::Int64 )
+end
+    return x   # nodes and weights in tuple. 
+end
+
+function asy( n::Int64 )
+# COMPUTE GAUSS-LEGENDRE NODES AND WEIGHTS USING ASYMPTOTIC EXPANSIONS. COMPLEXITY O(n). 
+    # Nodes and weights:
+    m = (mod(n,2)==0) ? n>>1 : (n+1)>>1
+    a = besselZeroRoots(m)./(n + 0.5)
+    x = legpts_nodes(n, a); 
+    w = legpts_weights(n, m, a)
+    # Use symmetry to get the others:
+    if ( mod(n, 2) == 1 )
+        x = [-x[1:end-1] ; 0 ; x[end-1:-1:1] ]
+        w = [w ; w[end-1:-1:1] ]
+    else
+        x = [-x ; x[end:-1:1] ]
+        w = [w ; w[end:-1:1] ]
+    end
+    return x, w
+end
+
+function legpts_nodes(n::Int64, a::Array{Float64})
+# ASYMPTOTIC EXPANSION FOR THE GAUSS-LEGENDRE NODES.  
+    vn = 1./(n + 0.5)
+    u = cot(a)
+    return cos( a + ((u-1./a)/8*vn^2) + 
+               ((n <= 3950) ? (6(1+u.^2)./a + 25./a.^3 - u.*(31u.^2+33))/384 : 0.0)*vn^4 + 
+               ((n <= 255) ?  u.*(2595 + 6350*u.^2 + 3779*u.^4)/15360 -1073/5120./a.^5 + 
+                            (1+u.^2).*(-(31*u.^2 + 11)/1024./a + u/512./a.^2 + -25/3072./a.^3) : 0.0)*vn^6 )
+end
+
+function legpts_weights(n::Int64, m::Int64, a::Array{Float64})
+# ASYMPTOTIC EXPANSION FOR THE GAUSS-LEGENDRE WEIGHTS.
+    vn = 1./(n + 0.5)
+    u = cot(a)
+    ua = cot(a).*a
+    # Assemble coefficients:
+    W1 = (n <= 850000) ? (ua + a.^2 - 1)./a.^2/8 : 0.0
+    W2 = ( n <= 1500 ) ? ( 81 - 31*ua - 3*(1-2*u.^2).*a.^2 + 6*u.*a.^3 - 
+                               (27 + 84*u.^2 + 56*u.^4).*a.^4 )./a.^4/384 : 0.0
+    W3 = ( n <= 170 ) ? (187/96*u.^4 + 295/256*u.^2 + 151/160*u.^6 + 153/1024) + 
+                      (-119/768*u.^2 -35/384*u.^4 - 65/1024).*u./a + 
+                      (5/512 + 7/384*u.^4 + 15/512*u.^2)./a.^2 + 
+                      (u.^2/512 - 13/1536).*u./a.^3 + 
+                      (-7/384*u.^2 + 53/3072)./a.^4 +
+                      (3749/15360*u)./a.^5 -1125/1024./a.^6 : 0.0        
+    return 2./(besselJ1(m).*(a./sin(a)).*(1./vn^2 + W1 + W2*vn.^2 + W3.*vn^4))       
+end
+
+function rec( n::Int64 ) 
+# COMPUTE GAUSS-LEGENDRE NODES AND WEIGHTS USING NEWTON'S METHOD. THREE-TERM RECURENCE 
+# IS USED FOR EVALUATION. COMPLEXITY O(n^2). 
+    hN = mod(n,2)
+    # Initial guesses: 
+    x0 = asy(n)[1]; x = x0[(n-hN)/2+1:n]   
+    # Perform Newton to find zeros of Legendre polynomial:
+    PP = innerRec( n, x ); dx = -PP[1]./PP[2]; x += dx
+    # One more Newton for derivatives: 
+    PP = (n<45)? innerRec( n, x ) : PP
+    # Use symmetry to get the other Legendre nodes and weights: 
+    nodes = vcat( -x[(n+hN)/2:-1:hN+1] , x )
+    weights = 2./((1-nodes.^2).*vcat( PP[2][(n+hN)/2:-1:1+hN] , PP[2] ).^2)
+    return nodes, weights
+end
+
+function innerRec( n::Int64, x )
+# EVALUATE LEGENDRE AND ITS DERIVATIVE USING THREE-TERM RECURRENCE RELATION.
+    Pm2, Pm1, PPm1, PPm2 = ones(x), x, ones(x), zeros(x) 
+    for k = 1:n-1
+        Pm2, Pm1 = Pm1, (((2k+1)/(k+1))*Pm1.*x - (k./(k+1))*Pm2)
+        PPm2, PPm1 = PPm1, (((2k+1)/(k+1))*(Pm2 + x.*PPm1)-(k/(k+1))*PPm2)
+    end
+    return Pm1, PPm1 
+end
+
+
+
+function besselZeroRoots(m::Int64)
+#BESSEL0ROOTS    ROOTS OF BESSELJ(0,x). USE ASYMPTOTICS. 
+    # Use McMahon's expansion for the remainder (NIST, 10.21.19):
+    jk = Array(Float64,m)
+    p = [1071187749376/315 0.0 -401743168/105 0.0 120928/15 0.0 -124/3 0.0 1.0 0.0]
+    ak = pi*( [1:m] -.25 )
+    ak82 = (.125./ak).^2   
+    # First 20 are precomputed:
+    twenty = [2.4048255576957728, 5.5200781102863106, 8.6537279129110122,
+                11.791534439014281, 14.930917708487785, 18.071063967910922,
+                21.211636629879258, 24.352471530749302, 27.493479132040254,
+                30.634606468431975, 33.775820213573568, 36.917098353664044,
+                40.058425764628239, 43.199791713176730, 46.341188371661814,
+                49.482609897397817, 52.624051841114996, 55.765510755019979,
+                58.906983926080942, 62.048469190227170]    
+    jk[1:min(m,20)] = twenty[1:min(m,20)]
+    if m >= 21
+        kk = [21:min(m,47)]
+        akcut = ak[kk]
+        ak82cut = ak82[kk]
+        jk[kk] = akcut + .125./akcut.*(1 + ak82cut.*(p[7] + ak82cut.*(p[5] + ak82cut.*p[3])))
+    end
+    if m >= 48
+        kk = [48:min(m,344)]
+        akcut = ak[kk]
+        ak82cut = ak82[kk]
+        jk[kk] = akcut + .125./akcut.*(1 + ak82cut.*(p[7]+ ak82cut.*p[5]))
+    end
+    if m >= 345
+        kk = [345:min(m,13191)]
+        akcut = ak[kk]
+        ak82cut = ak82[kk]
+        jk[kk] = akcut + .125./akcut.*(1 + ak82cut.*p[7])
+    end
+    if m >= 13192
+        jk[13192:m] = ak[13192:m] + .125./ak[13192:m]
+    end
+    return jk
+end
+
+function besselJ1( m::Int64 )
+# BESSELJ1 EVALUATE BESSELJ(1,x)^2 AT ROOTS OF BESSELJ(0,x). USE ASYMPTOTICS. 
+    # Use Taylor series of (NIST, 10.17.3) and McMahon's expansion (NIST, 10.21.19):
+    Jk2 = Array(Float64,m)
+    c = [-171497088497/15206400, 461797/1152, -172913/8064, 151/80, -7/24, 0.0, 2.0]
+    ak = pi*([1:m]-.25)
+    ak2 = (1./ak).^2
+    # First 10 are precomputed: 
+    ten = [0.2695141239419169, 0.1157801385822037, 0.07368635113640822,
+                 0.05403757319811628, 0.04266142901724309, 0.03524210349099610,
+                 0.03002107010305467, 0.02614739149530809, 0.02315912182469139, 0.02078382912226786] 
+    Jk2[1:min(m,10)] = ten[1:min(m,10)]
+    if m >= 11 
+        kk = [11:min(m,15)]
+        akcut = pi*(kk-.25)
+        ak2cut = (1./akcut).^2
+        Jk2[kk] = 1./(pi*akcut).*(c[7] + ak2cut.^2.*(c[5] + ak2cut.*(c[4] +
+                         ak2cut.*(c[3] + ak2cut.*(c[2]+ak2cut.*c[1])))))
+    end
+    if m >= 16
+        kk = [16:min(m,21)]
+        akcut = ak[kk]
+        ak2cut = ak2[kk]
+        Jk2[kk] = 1./(pi*akcut).*(c[7] + ak2cut.^2.*(c[5] + ak2cut.*(c[4] +
+                         ak2cut.*(c[3] + ak2cut.*c[2]))))
+    end
+    if m >= 22
+        kk = [22:min(m,55)]
+        akcut = ak[kk]
+        ak2cut = ak2[kk]
+        Jk2[kk] = 1./(pi*akcut).*(c[7] + ak2cut.^2.*(c[5] + ak2cut.*(c[4] + ak2cut.*c[3])))
+    end
+    if m >= 56 
+        kk = [56:min(m,279)]
+        akcut = ak[kk]
+        ak2cut = ak2[kk]
+        Jk2[kk] = 1./(pi*akcut).*(c[7] + ak2cut.^2.*(c[5] + ak2cut.*c[4]))
+    end
+    if m >= 280
+        kk = [280:min(m,2279)]
+        akcut = ak[kk]
+        ak2cut = ak2[kk]
+        Jk2[kk] = 1./(pi*akcut).*(c[7] + ak2cut.^2.*c[5])
+    end
+    if m >= 2280 
+        kk = [2280:m]
+        akcut = ak[kk]
+        ak2cut = ak2[kk]
+        Jk2[kk] = 1./(pi*akcut).*c[7]
+    end 
+    return Jk2
+end
