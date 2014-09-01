a87f237c6ce99f3ad32afd7cf903a3dd49b2b82a
AT <townsend@maths.ox.ac.uk>
2014-08-31 20:21:57 -0400

    Add code for fast Gauss-Legendre-Radau.

---
+++ src/GaussRadau.jl
@@ -0,0 +1,17 @@
+function GaussRadau( n::Int64 )
+#RADAUPTS   Gauss-Legendre-Radau Quadrature Nodes and Weights
+
+    if ( n == 1 )
+        x = (-1.0, 2.0)
+    elseif ( n == 2 )
+        x = ([-1.0, 1/3], [.5, 1.5])
+    else
+    # Compute via GaussJacobi:
+        x = GaussJacobi( n - 1, 0.0, 1.0 )
+        w = x[2]; x = x[1];
+        x = [-1, x]
+        w = [2/n^2, w./(1 + x[2:end])]
+        x = (x, w)
+    end
+    return x
+end
