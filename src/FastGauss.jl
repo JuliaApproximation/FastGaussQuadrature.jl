ef2185e73ebcb461357436f5eaec388c12ce5400
AT <townsend@maths.ox.ac.uk>
2014-08-31 20:32:37 -0400

    Add main module file to allow main functions to be exported.

--- src/FastGauss.jl
+++ src/FastGauss.jl
@@ -3,7 +3,14 @@ module FastGauss
 
 export GaussLegendre 
 export GaussChebyshev 
+export GaussJacobi
+export GaussLobatto
+export GaussRadau
 
 include("GaussLegendre.jl")
 include("GaussChebyshev.jl")
+include("GaussJacobi.jl")
+include("GaussLobatto.jl")
+include("GaussRadau.jl")
+
 end
