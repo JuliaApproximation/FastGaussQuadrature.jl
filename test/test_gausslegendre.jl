e90a4a72e3124bb0881b8da06ba12352d8a15550
Sheehan Olver <solver@mac.com>
2015-12-09 09:40:16 +1100

    • Updated README, much better timings! • Added currently breaking tests

--- test/test_gausslegendre.jl
+++ test/test_gausslegendre.jl
@@ -7,6 +7,8 @@ x, w = gausslegendre(n)
 @test abs(x[37] - 0.910959724904127) < tol
 @test abs(w[37] - 0.030479240699603) < tol
 
+@test_approx_eq dot( w,(x.^2)) 2/3
+
 # Test a larger n (using ASY)
 n = 251
 x, w = gausslegendre(n)
@@ -14,14 +16,20 @@ x, w = gausslegendre(n)
 @test abs(x[37] + 0.896467746955729) < tol
 @test abs(w[37] - 0.005535005742012) < tol
 
+@test_approx_eq dot( w,(x.^2)) 2/3
+
 x, w = gausslegendre(1013)
 @test norm(x[2] - -0.999985167586110, Inf) < tol
 @test norm(x[13] - -0.999218995240887, Inf) < tol
 @test norm(w[2] - 1.681691163200592e-05, Inf) < tol
 @test norm(w[13] - 1.224755309137936e-04, Inf) < tol
 
+@test_approx_eq dot( w,(x.^2)) 2/3
+
 x, w = gausslegendre(10013)
 @test norm(x[2] - -0.999999848054223, Inf) < tol
 @test norm(x[13] - -0.999991998242661, Inf) < tol
 @test norm(w[2] - 1.722757320118474e-07, Inf) < tol
 @test norm(w[13] - 1.254980540032470e-06, Inf) < tol
+
+@test_approx_eq dot( w,(x.^2)) 2/3
--- test/test_gausslobatto.jl
+++ test/test_gausslobatto.jl
@@ -1,9 +1,11 @@
-    # Test for GaussLobatto() 
-    tol = 1e-14
-    n = 42
-    x = gausslobatto(n)
-    w = x[2]; x = x[1]
-    @test ( length(x) == n) && ( length(w) == n )   
-    @test abs(x[37] - 0.922259214258616) < tol
-    @test abs(w[37] - 0.029306411216166) < tol
-    @test ( x[1] == -1 && x[n] == 1 )
+# Test for GaussLobatto()
+tol = 1e-14
+n = 42
+x = gausslobatto(n)
+w = x[2]; x = x[1]
+@test ( length(x) == n) && ( length(w) == n )
+@test abs(x[37] - 0.922259214258616) < tol
+@test abs(w[37] - 0.029306411216166) < tol
+@test ( x[1] == -1 && x[n] == 1 )
+
+@test_approx_eq dot( w,(x.^2)) 2/3
