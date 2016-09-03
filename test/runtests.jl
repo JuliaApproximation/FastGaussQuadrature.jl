c5c7671c68ffa47c35a1b0dbc42e39b479da7a62
Sheehan Olver <solver@mac.com>
2016-09-04 09:44:49 +1000

    add print statements to tests to help Travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,19 @@
 using Base.Test
 using FastGaussQuadrature
 
+println("Chebyshev tests")
 include("test_gausschebyshev.jl")
+println("Legendre tests")
 include("test_gausslegendre.jl")
+println("Jacobi tests")
 include("test_gaussjacobi.jl")
+println("Radau tests")
 include("test_gaussradau.jl")
+println("Lobatto tests")
 include("test_gausslobatto.jl")
+println("Bessel roots tests")
 include("test_besselroots.jl")
+println("Laguerre tests")
 include("test_gausslaguerre.jl")
+println("Hermite tests")
 include("test_gausshermite.jl")
