13af56c986982e368a860e559c10ecfae6511a38
Sheehan Olver <solver@mac.com>
2020-09-15 11:11:04 +0100

    Support Jacobi parameters in gaussradau

--- src/gaussradau.jl
+++ src/gaussradau.jl
@@ -1,17 +1,18 @@
-function gaussradau(n::Integer)
+function gaussradau(n::Integer, a=0.0, b=0.0)
+    T = float(promote_type(eltype(a), eltype(b)))
     # RADAUPTS   Gauss-Legendre-Radau Quadrature Nodes and Weights
     if n == 1
-        [-1.0], [2.0]
+        T[-1], T[2]
     elseif n == 2
-        [-1.0, 1/3], [.5, 1.5]
+        [-1, one(T)/3], [one(T)/2, convert(T,3)/2]
     else
         # Compute via GaussJacobi:
-        x, w = gaussjacobi(n - 1, 0.0, 1.0)
+        x, w = gaussjacobi(n - 1, a, b+1)
         @inbounds for i in 1:length(w)
-            w[i] = w[i] / (1.0 + x[i])
+            w[i] = w[i] / (1 + x[i])
         end
-        pushfirst!(x, -1.0)
-        pushfirst!(w, 2.0 / n^2)
+        pushfirst!(x, -1)
+        pushfirst!(w, convert(T, 2) / n^2)
         x, w
     end
 end
--- test/test_gaussradau.jl
+++ test/test_gaussradau.jl
@@ -1,24 +1,39 @@
+using FastGaussQuadrature, Test
+
 @testset "Gauss–Radau" begin
-    n = 1
-    x, w = gaussradau(n)
-    @test x[1] ≈ -1.
-    @test w[1] ≈ 2.
+    @testset "Legendre" begin
+        n = 1
+        x, w = gaussradau(n)
+        @test x[1] ≈ -1.
+        @test w[1] ≈ 2.
 
-    n = 2
-    x, w = gaussradau(n)
-    @test x[1] ≈ -1.
-    @test x[2] ≈ 1/3
-    @test w[1] ≈ .5
-    @test w[2] ≈ 1.5
+        n = 2
+        x, w = gaussradau(n)
+        @test x[1] ≈ -1.
+        @test x[2] ≈ 1/3
+        @test w[1] ≈ .5
+        @test w[2] ≈ 1.5
 
-    tol = 1e-14
-    n = 42
-    ntests = 4
-    x, w = gaussradau(n)
-    @test length(x) == n && length(w) == n
-    @test abs(x[37] - 0.908847278001044) < tol
-    @test abs(w[37] - 0.031190846817016) < tol
-    @test x[1] == -1
+        tol = 1e-14
+        n = 42
+        ntests = 4
+        x, w = gaussradau(n)
+        @test length(x) == n && length(w) == n
+        @test abs(x[37] - 0.908847278001044) < tol
+        @test abs(w[37] - 0.031190846817016) < tol
+        @test x[1] == -1
 
-    @test dot( w,exp.(x)) ≈ exp(1)-exp(-1)
+        @test dot( w,exp.(x)) ≈ exp(1)-exp(-1)
+    end
+    @testset "Jacobi" begin
+        a,b = 0.1, 0.2
+        # compare with Gauss–Jacobi
+        for n = 1:5
+            x, w = gaussradau(n, a, b)
+            x̃, w̃ = gaussjacobi(n, a, b)
+            @test sum(w) ≈ sum(w̃)
+            @test dot(w,x) ≈ dot(w̃,x̃)
+            @test dot(w,x.^2) ≈ dot(w̃,x̃.^2)
+        end
+    end
 end
