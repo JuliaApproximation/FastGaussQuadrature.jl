530745feaf78fcebfe363a15ffbfde0935d1ef97
hyrodium <hyrodium@gmail.com>
2021-01-07 23:47:56 +0900

    add docstring and jldoctest for gaussradau

--- src/gaussradau.jl
+++ src/gaussradau.jl
@@ -1,7 +1,32 @@
-"""
-   gaussradau(n)
+@doc raw"""
+    gaussradau(n::Integer) -> Tuple{Vector{Float64},Vector{Float64}}
 
-Creates the n-point Gauss-Radau quadrature rule, with the first node fixed at -1.
+Return nodes and weights of [Gauss-Radau quadrature](https://mathworld.wolfram.com/RadauQuadrature.html).
+
+```math
+\int_{-1}^{1} f(x) dx \approx \sum_{i=1}^{n} w_i f(x_i)
+```
+
+# Examples
+```jldoctest; setup = :(using FastGaussQuadrature, LinearAlgebra)
+julia> x, w = gaussradau(3);
+
+julia> f(x) = x^4;
+
+julia> I = dot(w, f.(x));
+
+julia> I ≈ 2/5
+true
+```
+
+Note that the first node is fixed at -1.
+
+```jldoctest; setup = :(using FastGaussQuadrature, LinearAlgebra)
+julia> x, w = gaussradau(3);
+
+julia> x[1]
+-1.0
+```
 """
 function gaussradau(n::Integer, T::Type=Float64)
     a = b = zero(T)
