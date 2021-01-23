5f92d2b2c10a97efbc40e4a7efee4e50d48badb5
hyrodium <hyrodium@gmail.com>
2021-01-23 12:40:54 +0900

    add test for besselroots

--- test/test_besselroots.jl
+++ test/test_besselroots.jl
@@ -3,10 +3,12 @@ import SpecialFunctions: besselj
 
 @testset "Bessel Roots" begin
     @test_throws DomainError approx_besselroots(0.0, -1)
- 
-    tol = 1e-11
+
+    msg = "`besselroots` has been renamed to `approx_besselroots`, and `besselroots` will be removed in the next major release."
+    @test_logs (:warn, msg) besselroots(0.2,3)
 
     # Check if besselj(ν, approx_besselroots(ν, n) ) is small:
+    tol = 1e-11
 
     for ν = 0.:0.1:5.
         n = 10
