3915029315c59174bebdb28b202daed74f47680a
Yuto Horikawa <hyrodium@gmail.com>
2023-09-15 10:42:37 +0900

    deprecate besselroots (#124)

--- test/test_besselroots.jl
+++ test/test_besselroots.jl
@@ -1,8 +1,7 @@
 @testset "Bessel Roots" begin
     @test_throws DomainError approx_besselroots(0.0, -1)
 
-    msg = "`besselroots` has been renamed to `approx_besselroots`, and `besselroots` will be removed in the next major release."
-    @test_logs (:warn, msg) besselroots(0.2,3)
+    @test_deprecated besselroots(0.2,3)
 
     # Check if besselj(ν, approx_besselroots(ν, n) ) is small:
     tol = 1e-11
