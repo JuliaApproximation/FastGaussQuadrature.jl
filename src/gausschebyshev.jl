d3b15246c97f93c8f6d24bdccc14482e8a673f78
Oscar Smith <oscardssmith@gmail.com>
2026-04-28 15:52:37 -0400

    Update src/gausschebyshev.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/gausschebyshev.jl
+++ src/gausschebyshev.jl
@@ -59,7 +59,7 @@ gausschebyshevu(n::Integer) = gausschebyshevu(Float64, n)
 @doc raw"""
     gausschebyshevv([T=Float64,] n::Integer) -> x, w  # nodes, weights
 
-Return nodes `x` and weights `w` of [Gauss-Chebyshev quadrature](https://en.wikipedia.org/wiki/Chebyshev%E2%80%93Gauss_quadrature) of the 3rd kind and type T.
+Return nodes `x` and weights `w` of [Gauss-Chebyshev quadrature](https://en.wikipedia.org/wiki/Chebyshev%E2%80%93Gauss_quadrature) of the 3rd kind and type `T`.
 
 ```math
 \int_{-1}^{1} f(x)\sqrt{\frac{1+x}{1-x}} dx \approx \sum_{i=1}^{n} w_i f(x_i)
